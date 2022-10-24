7fbb98dcde3c99a79287b5be3533fff65248855c
Lucas Ulerich <lucas.ulerich@to-brasil.com>
2022-10-24 17:59:13 -0300

    Aulas do modulo 1 - Fundamentos

---
+++ javascript/Fundamentos/organizacao.js
@@ -0,0 +1,18 @@
+console.log('Linha de código') // Esse é um exemplo de sentença de codigo
+
+console.log('Outra linha de código')
+// Em javascript não é obrigatorio o ponto e virgula.
+
+function bloco() {
+  console.log('Esse é um bloco de código')
+  console.log('pode ter varias linhas dentro dele')
+}
+
+bloco()
+// Em javascript o delimitador de um bloco de código são as chaves.
+
+/*
+  {Inicio
+
+  Fim}
+*/
