650950bfc8559e83b7829c416079985cefe1cf45
Lucas Ulerich <lucas.ulerich@to-brasil.com>
2022-10-27 14:31:28 -0300

    Adicionando aulas do modulo fundamentos

---
+++ javascript/Fundamentos/comentario.js
@@ -0,0 +1,16 @@
+// Comentario de uma linha
+
+console.log('Linha 1') //comentarios não afetam o código
+
+/*
+*comentarios de múltiplas linhas
+* 
+*/
+
+/*
+também pode ser feito
+dessa 
+forma 
+*/
+
+ 
---
+++ javascript/Fundamentos/variaveisEConstantes.js
@@ -0,0 +1,18 @@
+var a = 3
+let b = 4
+/**
+ * var é a palavra reservada para declarar uma variavel
+ * a = é o nome da variavel, a unica regra é não usar palavra reservada
+ * O simbulo de igual é atribuição (a recebe 3)
+ *Seguido do valor
+ *  */ 
+
+ // CRIE VARIAVEIS USANDO LET
+
+ let nome = 'lucas' // Pode mudar o valor durante a execução
+ const peso = 52.3 // Nunca muda de valor
+
+ var a = 30
+  b = 40
+
+ console.log(a,b)
